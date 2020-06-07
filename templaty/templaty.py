6dc36b93b46c461e41c42fe1d840b8c0c5c61812
amannocci <adrien.mannocci@gmail.com>
2020-06-07 21:29:32 +0200

    [Added] Support jinja2 rendering
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- templaty/templaty.py
+++ templaty/templaty.py
@@ -1,6 +1,20 @@
+import os
+
 import click
+from jinja2 import Environment, FileSystemLoader
+from path import Path
 
 
-@click.command()
-def main():
-    print('Hello world')
+@click.command('templaty')
+@click.argument('input_file')
+def main(input_file):
+    # Check template path is a regular file
+    template_path = Path(input_file)
+    if not template_path.isfile():
+        raise click.FileError(template_path.abspath(), 'Must be a regular file')
+
+    env = Environment(
+        loader=FileSystemLoader(template_path.parent.abspath()),
+    )
+    template = env.get_template(str(template_path.name))
+    click.echo(template.render(**os.environ))
--- tests/unit/templaty_test.py
+++ tests/unit/templaty_test.py
@@ -3,7 +3,22 @@ from click.testing import CliRunner
 from templaty.templaty import main
 
 
-def test_main():
+def test_no_template():
     runner = CliRunner()
     result = runner.invoke(main)
+    assert result.exit_code == 2
+    assert 'Missing argument' in result.stdout
+
+
+def test_wrong_template():
+    runner = CliRunner()
+    result = runner.invoke(main, args=['unknown'])
+    assert result.exit_code == 1
+    assert 'Must be a regular file' in result.stdout
+
+
+def test_simple_template():
+    runner = CliRunner()
+    result = runner.invoke(main, args=['tests/fixtures/simple.tpl'], env={'simple': '1'})
     assert result.exit_code == 0
+    assert result.output == 'Hello world: 1\n'

40c4eb754249d0d438d652caaf301e5d15bc7108
Adrien Mannocci <adrien.mannocci@gmail.com>
2023-03-26 13:50:32 +0200

    Use github actions instead of TravisCI (#17) (Closes #16)
    * [Added] Initial github actions support
    
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>
    
    * [Fixed] Environment configuration
    
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>
    
    * [Fixed] Correct comment docs
    
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>
    
    ---------
    
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- temply/filters.py
+++ temply/filters.py
@@ -5,27 +5,72 @@ from jinja2 import pass_context, pass_eval_context
 
 
 @pass_eval_context
-def from_json(ctx, value):
+def from_json(_, value):
+    """
+    Parse json value.
+    Args:
+        _: any.
+        value: value to parse.
+
+    Returns:
+        parsed value.
+    """
     return json.loads(value)
 
 
 @pass_eval_context
-def to_json(ctx, value):
+def to_json(_, value):
+    """
+    Convert any value to json.
+    Args:
+        _: any.
+        value: value to convert.
+
+    Returns:
+        converted value.
+    """
     return json.dumps(value).strip()
 
 
 @pass_eval_context
-def from_yaml(ctx, value):
+def from_yaml(_, value):
+    """
+    Parse yaml value.
+    Args:
+        _: any.
+        value: value to parse.
+
+    Returns:
+        parsed value.
+    """
     return yaml.safe_load(value)
 
 
 @pass_eval_context
-def to_yaml(ctx, value):
+def to_yaml(_, value):
+    """
+    Convert any value to yaml.
+    Args:
+        _: any.
+        value: value to convert.
+
+    Returns:
+        converted value.
+    """
     return yaml.safe_dump(value).strip()
 
 
 @pass_context
 def get_environment(ctx, prefix=""):
+    """
+    Filter environment variables with a prefix.
+    Args:
+        ctx: context.
+        prefix: environment variable prefix.
+
+    Returns:
+        generator with all environment variables with a prefix.
+    """
     for key, value in sorted(ctx.items()):
         if not callable(value) and key.startswith(prefix):
             yield key[len(prefix) :], value

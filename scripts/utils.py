bfb9fe6d1f10ae6dd25798d096d4b2fd3118834f
Adrien Mannocci <adrien.mannocci@gmail.com>
2023-10-20 01:15:53 +0200

    fix: correct multi arch build (#20)
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, Final
 
 import toml
 from dotty_dict import dotty
+from sh import Command, CommandNotFound
 from toml import TomlDecodeError
 
 
@@ -30,3 +31,26 @@ def read_project_conf() -> Dict[str, Any]:
     except TomlDecodeError as err:
         print("The `pyproject.toml` file isn't valid", file=sys.stderr)
         raise err
+
+
+def container_backend() -> Command:
+    """
+    Try to detect a container backend.
+    Either podman or docker.
+
+    Returns:
+        a command if a backend is detected.
+    Raises:
+        CommandNotFound: if a suitable backend can't be found.
+    """
+    cmd = None
+    for backend in ["docker", "podman"]:
+        try:
+            cmd = Command(backend)
+            continue
+        except CommandNotFound:
+            pass
+
+    if not cmd:
+        raise CommandNotFound("Unable to find a suitable backend: docker or podman")
+    return cmd

cd0f33194a0356c8c688d198501bc066e8f6dba2
amannocci <adrien.mannocci@gmail.com>
2024-04-20 19:41:03 +0200

    feat: support for pyright
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- scripts/env.py
+++ scripts/env.py
@@ -1,13 +1,25 @@
+import json
 import sys
+from pathlib import Path
 
-from sh import CommandNotFound, poetry
+from sh import Command, CommandNotFound, RunningCommand
+
+from scripts.utils import Constants, detect_poetry
 
 
 def configure() -> None:
-    poetry("install", _out=sys.stdout, _err=sys.stderr)
-    try:
-        from sh import pre_commit
+    poetry = detect_poetry()
 
+    # Generate pyright config
+    process: RunningCommand = poetry("env", "info", "-p", _err=sys.stderr, _return_cmd=True)  # type: ignore[no-untyped-def]
+    project_venv_path = Path(process.stdout.decode(Constants.ENCODING_UTF_8).strip())
+    project_venv_name = project_venv_path.name
+    venv_path = project_venv_path.parents[0].as_posix()
+    if not Constants.PYRIGHTCONFIG_PATH.exists():
+        Constants.PYRIGHTCONFIG_PATH.write_text(json.dumps({"venv": project_venv_name, "venvPath": venv_path}))
+
+    try:
+        pre_commit = Command("pre-commit")
         pre_commit("install", _out=sys.stdout, _err=sys.stderr)
     except (CommandNotFound, ImportError):
         print("pre-commit isn't installed")
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from pathlib import Path
-from typing import Any, Dict, Final
+from typing import Any, Dict, Final, NoReturn
 
 import toml
 from dotty_dict import dotty
@@ -15,7 +15,16 @@ class Constants:
     # pylint: disable=R0903
     ENCODING_UTF_8: Final[str] = "utf-8"
     PROJECT_PATH: Final[Path] = Path("pyproject.toml")
+    PYRIGHTCONFIG_PATH: Final[Path] = Path("pyrightconfig.json")
     REGISTRY_URL: str = os.getenv("REGISTRY_URL", "local.dev")
+
+
+def fatal(msg: str, err: Exception | None = None) -> NoReturn:
+    """Print error message on stderr and die."""
+    print(msg, file=sys.stderr)
+    if err:
+        print(err, file=sys.stderr)
+    sys.exit(1)
 
 
 def read_project_conf() -> Dict[str, Any]:
@@ -31,6 +40,18 @@ def read_project_conf() -> Dict[str, Any]:
     except TomlDecodeError as err:
         print("The `pyproject.toml` file isn't valid", file=sys.stderr)
         raise err
+
+
+def detect_poetry() -> Command:
+    """
+    Try to detect poetry.
+    Returns:
+        a command if poetry is detected.
+    """
+    try:
+        return Command("poetry")
+    except CommandNotFound:
+        fatal("`poetry` isn't detected")
 
 
 def container_backend() -> Command:
